c30313b00d9c1241fc95e7e48adfbf47cc064c4f
emir2612 <emirkerestecioglu@hotmail.com>
2025-05-12 22:31:25 +0300

    Update saveCountry.js

--- saveCountry.js
+++ saveCountry.js
@@ -1,9 +1,1 @@
 
-exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body);
-  console.log("Kaydedilen veri:", data);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Veri başarıyla kaydedildi." }),
-  };
-};
